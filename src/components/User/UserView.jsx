8870e1c16aefa7f2c7a18b74175e182667ee0baa
Gaspar <gaspargomez2000@outlook.com>
2023-08-02 19:34:32 -0300

    excel sheet

--- src/components/User/UserView.jsx
+++ src/components/User/UserView.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
-import FirebaseApp from "../../firebase";
+import FirebaseApp, { auth } from "../../firebase";
+import { getAuth } from "firebase/auth";
 
 export function UserView() {
   const firestore = getFirestore(FirebaseApp);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [authUserEmail, setAuthUserEmail] = useState(null); // State to store the authenticated user's email
+  const auth = getAuth(); // Get the Firebase Auth instance
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,7 +17,7 @@ export function UserView() {
         const querySnapshot = await getDocs(collection(firestore, "driveurl"));
         const fetchedData = querySnapshot.docs.map((doc) => ({
           id: doc.id,
-          url: doc.data().url,
+          driveurl: doc.data().driveurl,
           email: doc.data().email,
         }));
         setData(fetchedData);
@@ -25,30 +28,40 @@ export function UserView() {
       }
     };
     fetchData();
-  }, []);
+  }, [firestore]);
+
+  useEffect(() => {
+    // Fetch the authenticated user's email when the component mounts
+    const user = auth.currentUser;
+    if (user) {
+      setAuthUserEmail(user.email);
+    }
+  }, [auth]);
 
   const DriveURL = ({ email }) => {
     const usuario = data.find((user) => user.email === email);
-
-    if (usuario) {
-      return <iframe src={usuario.url}></iframe>;
-    } else {
-      return <h2>No se encontró URL para el usuario</h2>;
+    
+    if (usuario && email === authUserEmail) {
+      return <iframe className="w-full h-96" src={usuario.driveurl}></iframe>;
+      
     }
+    
+    return (null); // Return null if user not authenticated or email does not match
   };
 
   return (
     <div>
-      <h1>Imágenes de usuarios</h1>
       {loading ? (
         <p>Cargando...</p>
       ) : (
         data.map((user) => (
           <div key={user.id}>
-            <h3>{user.email}</h3>
             <DriveURL email={user.email} />
           </div>
         ))
+      )}
+      {!loading && !data.some(user => user.email === authUserEmail) && (
+        <h2>No se encontró URL para el usuario</h2>
       )}
     </div>
   );
